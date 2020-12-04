1dac7bc7b865aaeacd7ae4e4248d107271a2de32
Rute Souza de Abreu <rute.s.abreu@gmail.com>
2020-12-04 20:30:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,14 @@ app.get('/', (_, response) =>{
     return response.send("'I'm Working")
 })
 
-app.post('/read', function(request, response){
+app.post('/post', function(request, response){
     console.log(request.body)
     response.send(request.body)
 });
+
+app.get('/get', (_, response) =>{
+    return response.send("'I'm Working")
+})
+
 
 app.listen(8080, () => console.log("Test"))
