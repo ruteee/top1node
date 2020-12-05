6891ba34808901d7fac436db97e2cf56b39ec2a3
Rute Souza de Abreu <rute.s.abreu@gmail.com>
2020-12-04 21:48:29 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 const express  = require('express')
 const bodyParser = require('body-parser');
+const axios  = require('axios')
 
+url_poke = " https://pokeapi.co/api/v2/pokemon/ditto"
 
 const app = express();
-
-
 app.use(express.json())
 
 app.get('/', (_, response) =>{
     return response.send("'I'm Working")
 })
 
-app.post('/post', function(request, response){
+app.get('/get', async (_, response) =>{
+    let data = ""
+    try{
+        data = await get_pokemon()
+    }catch(err){
+        response.send(err.message)
+    }        
+    
+    console.log(data.data)
+    return response.send(data.data)
+})
+
+app.post('/read', function(request, response){
     console.log(request.body)
     response.send(request.body)
 });
 
-app.get('/get', (_, response) =>{
-    return response.send("'I'm Working")
-})
+async function get_pokemon(){
+    let data = ""
+    data = await axios.get(url_poke)
+    return data
+}
 
-
-app.listen(8080, () => console.log("Test"))
+app.listen(8080, () => console.log("App Inicializado"))
